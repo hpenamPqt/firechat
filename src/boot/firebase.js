f125abd9ca76f3819eb665728aedfa90e30b2ed7
Horacio Peña Mendoza <hpenam@uaemex.mx>
2022-10-04 19:12:10 -0500

    initial commit

---
+++ src/boot/firebase.js
@@ -0,0 +1,8 @@
+import { boot } from 'quasar/wrappers'
+
+console.log('firebase ok...')
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app, router, ... } */) => {
+  // something to do
+})
